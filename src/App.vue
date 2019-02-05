eb3ecab69b5b0135d6024358e2a805f0cbbe0d36
l08084 <l08084.1989@gmail.com>
2019-02-05 22:11:35 +0900

    アプリケーションシェル-2

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Tour of Heroes"/>
+    <h1>{{title}}</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      title: 'Tour of Heroes',
+    }
   }
-}
+};
 </script>
 
 <style>
-
+h1 {
+  color: #369;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 250%;
+}
+h2, h3 {
+  color: #444;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: lighter;
+}
+body {
+  margin: 2em;
+}
+body, input[type="text"], button {
+  color: #888;
+  font-family: Cambria, Georgia;
+}
+/* everywhere else */
+* {
+  font-family: Arial, Helvetica, sans-serif;
+}
 </style>
