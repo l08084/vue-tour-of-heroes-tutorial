5a930b848f7de78c31140e5f6de6f7ba1d48df81
l08084 <l08084.1989@gmail.com>
2019-02-09 18:33:09 +0900

    サービス-1

--- src/components/Heroes.vue
+++ src/components/Heroes.vue
@@ -15,23 +15,13 @@
 
 <script>
 import HeroDetail from './HeroDetail.vue'
+import axios from 'axios';
 
 export default {
     name: 'Heroes',
     data: function() {
     return {
-        heroes: [
-            { id: 11, name: 'Mr. Nice' },
-            { id: 12, name: 'Narco' },
-            { id: 13, name: 'Bombasto' },
-            { id: 14, name: 'Celeritas' },
-            { id: 15, name: 'Magneta' },
-            { id: 16, name: 'RubberMan' },
-            { id: 17, name: 'Dynama' },
-            { id: 18, name: 'Dr IQ' },
-            { id: 19, name: 'Magma' },
-            { id: 20, name: 'Tornado' }
-        ],
+        heroes: undefined,
         selectedHero: undefined,
         }
     },
@@ -42,6 +32,11 @@ export default {
     },
     components: {
         HeroDetail
+    },
+    beforeCreate: function () {
+        axios
+            .get('/mock-heroes.json')
+            .then((response) => this.heroes = response.data)
     }
 };
 </script>
