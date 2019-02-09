4111438b527dcc60587aa8ddc00ddd2c7c8d0ee6
l08084 <l08084.1989@gmail.com>
2019-02-09 18:58:01 +0900

    サービス-2

---
+++ src/components/Messages.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+</div>
+</template>
+
+<script>
+import HeroDetail from './HeroDetail.vue'
+import axios from 'axios';
+
+export default {
+    name: 'Heroes',
+};
+</script>
+
+<style scoped>
+</style>
