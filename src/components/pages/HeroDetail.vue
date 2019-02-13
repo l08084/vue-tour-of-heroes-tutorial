2efa09ed43a559bd3b49d92384854f85c1e6db90
l08084 <l08084.1989@gmail.com>
2019-02-13 21:46:32 +0900

    ルーティング-8

--- src/components/pages/HeroDetail.vue
+++ src/components/pages/HeroDetail.vue
@@ -10,10 +10,12 @@
             </label>
         </div>
     </div>
+    <button v-on:click="goBack()">go back</button>
 </div>
 </template>
 
 <script>
+import router from '../../router/index'
 import heroMixin from '../mixin/heroMixin'
 
 export default {
@@ -33,8 +35,43 @@ export default {
                 .then((response) =>
                     this.hero = response.data.find((h) =>
                         h.id === this.$route.params.id))
+        },
+        goBack: function () {
+            router.go(-1)
         }
     }
 };
 </script>
 
+<style scoped>
+label {
+  display: inline-block;
+  width: 3em;
+  margin: .5em 0;
+  color: #607D8B;
+  font-weight: bold;
+}
+input {
+  height: 2em;
+  font-size: 1em;
+  padding-left: .4em;
+}
+button {
+  margin-top: 20px;
+  font-family: Arial;
+  background-color: #eee;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 4px;
+  cursor: pointer;
+  cursor: hand;
+}
+button:hover {
+  background-color: #cfd8dc;
+}
+button:disabled {
+  background-color: #eee;
+  color: #ccc;
+  cursor: auto;
+}
+</style>
